2dd9552b821fa461274e3c6e707bf56d240befb2
isntitfun <butnothingstays@outlook.com>
2022-12-17 18:32:59 +0900

    edited PORT value

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import User from "./models/users";
 import Comment from "./models/comments.js";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const handleAppListen = () => {
   console.log(`✅ Listening on localhost:${PORT} :: No Error`);
 };
